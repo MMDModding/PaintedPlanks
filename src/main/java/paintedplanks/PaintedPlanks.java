40b0d71b81b85a2694d9c818c3dd82c26e66dcf6
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-04-30 23:52:05 -0700

    More mod skeleton

--- src/main/java/paintedplanks/PaintedPlanks.java
+++ src/main/java/paintedplanks/PaintedPlanks.java
@@ -1,8 +1,11 @@
 package paintedplanks;
 
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
@@ -16,6 +19,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 		updateJSON = "https://raw.githubusercontent.com/jriwanek/PaintedPlanks/master/update.json")
 public class PaintedPlanks
 {
+	public static PaintedPlanks INSTANCE = null;
     public static final String MODID = "paintedplanks";
     public static final String NAME = "Painted Planks";
     public static final String VERSION = "1.0.0";
@@ -23,15 +27,76 @@ public class PaintedPlanks
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+    	INSTANCE = this;
+
+    	// TODO: load config
+    	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+    	config.load();
+
+    	config.save();
+    	
+		paintedplanks.init.Blocks.init();
+		paintedplanks.init.Items.init();
+
+		if(event.getSide() == Side.CLIENT){
+			clientPreInit(event);
+		}
+		if(event.getSide() == Side.SERVER){
+			serverPreInit(event);
+		}
     }
+
+	@SideOnly(Side.CLIENT)
+	private void clientPreInit(FMLPreInitializationEvent event){
+		// client-only code
+	}
+	@SideOnly(Side.SERVER)
+	private void serverPreInit(FMLPreInitializationEvent event){
+		// server-only code
+	}
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+		paintedplanks.init.Recipes.init();
+
+		if(event.getSide() == Side.CLIENT){
+			clientInit(event);
+		}
+		if(event.getSide() == Side.SERVER){
+			serverInit(event);
+		}
     }
+
+	@SideOnly(Side.CLIENT)
+	private void clientInit(FMLInitializationEvent event){
+		// client-only code
+//		paintedblocks.init.Items.registerItemRenders(event);
+//		paintedblocks.init.Blocks.registerItemRenders(event);
+	}
+	@SideOnly(Side.SERVER)
+	private void serverInit(FMLInitializationEvent event){
+		// server-only code
+	}
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
+		if(event.getSide() == Side.CLIENT){
+			clientPostInit(event);
+		}
+		if(event.getSide() == Side.SERVER){
+			serverPostInit(event);
+		}
+
     }
+
+	@SideOnly(Side.CLIENT)
+	private void clientPostInit(FMLPostInitializationEvent event){
+		// client-only code
+	}
+	@SideOnly(Side.SERVER)
+	private void serverPostInit(FMLPostInitializationEvent event){
+		// server-only code
+	}
 }
